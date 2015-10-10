744d49f6d3310b8848780692791e241e53776180
diorge <diorge.nla@hotmail.com>
2015-10-09 21:13:27 -0300

    Parsing the argument

--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,25 @@ module Main
 
 import Rooks
 import Data.Matrix
+import Data.List.Split (splitOn)
 
 main :: IO ()
 main = undefined
 
 parse :: String -> Maybe Board
-parse = undefined
+parse = fmap fromLists . parse'
+
+parse' :: String -> Maybe [[Piece]]
+parse' = allJust . map parseRow . splitOn "|"
+
+allJust :: [Maybe [a]] -> Maybe [[a]]
+allJust [] = Just []
+allJust ((Just []):_) = Just []
+allJust (Nothing:_) = Nothing
+allJust ((Just x):xs) = fmap (x:) (allJust xs)
+
+parseRow :: String -> Maybe [Piece]
+parseRow []       = Just []
+parseRow ('.':xs) = fmap (Empty :) (parseRow xs)
+parseRow ('X':xs) = fmap (Pawn  :) (parseRow xs)
+parseRow _        = Nothing
